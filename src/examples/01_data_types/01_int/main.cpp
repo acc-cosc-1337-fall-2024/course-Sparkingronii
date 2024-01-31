f40db46d8352c9455567239baba32abc8fb4ebf8
AlexanderPowersACC <157078028+Sparkingronii@users.noreply.github.com>
2024-01-31 01:00:19 +0000

    1/30/2024

--- src/examples/01_data_types/01_int/main.cpp
+++ src/examples/01_data_types/01_int/main.cpp
@@ -1,5 +1,24 @@
+
+#include "int.h"
+#include<iostream>
+
+using std::cout;
+
 int main()
 {
+    cout<<"Echo: "<<echo_variable(10)<<"\n";
+
+    auto num = 10;
+
+    cout<<"Bytes required to store int: "<<sizeof(num)<<"\n";
+
+    auto decimal = 10.5;
+
+    cout<<"Bytes required to store double: "<<sizeof(decimal)<<"\n";
+
+    auto ch = 'A';
+
+    cout<<"Bytes required to store char: "<<sizeof(ch)<<"\n";
 
     return 0;
 }
