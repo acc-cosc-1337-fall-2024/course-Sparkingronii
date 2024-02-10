162001192cea2176e54c6c4763a9e03b0f479b3a
AlexanderPowersACC <157078028+Sparkingronii@users.noreply.github.com>
2024-02-10 15:58:50 +0000

    Homework 3(revision.1)

--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -6,20 +6,22 @@ using std::cin; using std::cout; using std::string;
 int main() 
 {
 	int Option;
-	cout<<"1-Letter grade using if"<<"\\n";
-	cout<<"2-Letter grade using switch"<<"\\n";
-	cout<<"3-Exit"<<"\\n";
+	cout<<"MENU"<<"\n";
+	cout<<"Option 1-Letter grade using if"<<"\n";
+	cout<<"Option 2-Letter grade using switch"<<"\n";
+	cout<<"Option 3-Exit"<<"\n";
 
 	cout<<"Choose an Option: ";
 	cin>>Option;
+	cout<<Option;
 
 	if (Option == 1)
-		{
+	{
 	
 		auto Grade = 0;
 		string result = "";
 
-		cout<<"Enter a Grade: ";
+		cout<<"Enter a Grade Between 0 and 100: ";
 		cin>>Grade;
 
 		result = get_letter_grade_using_if(Grade);
@@ -27,15 +29,15 @@ int main()
 		cout<<result<<"\n";
 
 		return 0;
-		}
+	}
 	
 	else if (Option == 2)
-		{
+	{
 	
 		auto Grade = 0;
 		string result = "";
 
-		cout<<"Enter a Grade: ";
+		cout<<"Enter a Grade Between 0 and 100: ";
 		cin>>Grade;
 
 		result =  get_letter_grade_using_switch(Grade);
@@ -43,7 +45,7 @@ int main()
 		cout<<result<<"\n";
 
 		return 0;
-		}
+	}
 
 	else if (Option == 3)
 	{
