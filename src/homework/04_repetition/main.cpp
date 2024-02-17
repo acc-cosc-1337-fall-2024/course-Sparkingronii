b0f3d0732eaf72be0741e997502a104c55c5ec9a
AlexanderPowersACC <157078028+Sparkingronii@users.noreply.github.com>
2024-02-17 14:43:58 +0000

    Homework 4 Initial

--- src/homework/04_repetition/main.cpp
+++ src/homework/04_repetition/main.cpp
@@ -1,8 +1,63 @@
 //write include statements
+#include<iostream>
+#include "repetition.h"
+using std::cin; using std::cout;
 
 //write using statements
 
 int main() 
 {
-	return 0;
+	int Option;
+	cout<<"MENU"<<"\n";
+	cout<<"Option 1-Factorial"<<"\n";
+	cout<<"Option 2-Greates Common Divisor"<<"\n";
+	cout<<"Option 3-Exit"<<"\n";
+
+	cout<<"Choose an Option: ";
+	cin>>Option;
+	cout<<Option<<"\n";
+
+	if (Option == 1)
+	{
+	
+		auto Number = 0;
+		auto result = 0;
+
+		cout<<"Enter a Number: ";
+		cin>>Number;
+
+		result = factorial(Number);
+
+		cout<<result<<"\n";
+
+		return 0;
+	}
+
+	if (Option == 2)
+	{
+	
+		auto Number1 = 0;
+		auto Number2 = 0;
+		auto result = 0;
+
+		cout<<"Enter a Number: ";
+		cin>>Number1;
+
+		cout<<"Enter a Second Number: ";
+		cin>>Number2;
+
+		result = gcd(Number1, Number2);
+
+		cout<<result<<"\n";
+
+		return 0;
+	}
+
+	else if (Option == 3)
+	{
+		auto choice = 'y';
+
+		if(choice == 'y' || 'Y')
+		return;
+	}
 }
--- src/homework/04_repetition/repetition.cpp
+++ src/homework/04_repetition/repetition.cpp
@@ -1,3 +1,31 @@
 //add include statements
+#include<iostream>
+#include "repetition.h"
 
 //add function(s) code here
+
+int factorial(int num)
+{
+    auto start = 1;
+    auto sum = 1;
+
+    while(num > 1)
+    {
+        sum *= start;
+        start --;
+    }
+
+    return sum;
+}
+
+int gcd(int num1, int num2)
+{
+    if (num1 == num2)
+    return num1;
+
+    else if(num1 > num2)
+    return gcd(num1 - num2, num2);
+
+    else //if(num1 < num2)
+    return gcd(num1, num2 - num1);
+}
