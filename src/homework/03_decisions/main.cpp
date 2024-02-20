b9891fb37b3ee7de7f3161ecb1124be9965f5ee4
AlexanderPowersACC <157078028+Sparkingronii@users.noreply.github.com>
2024-02-20 19:28:04 +0000

    Homework 3 Final Looping Revision

--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -5,53 +5,53 @@ using std::cin; using std::cout; using std::string;
 
 int main() 
 {
-	while ( 1 == true)
+	while (true)
 	{
-	int Option;
-	cout<<"MENU"<<"\n";
-	cout<<"Option 1-Letter grade using if"<<"\n";
-	cout<<"Option 2-Letter grade using switch"<<"\n";
-	cout<<"Option 3-Exit"<<"\n";
+		int Option;
+		cout<<"MENU"<<"\n";
+		cout<<"Option 1-Letter grade using if"<<"\n";
+		cout<<"Option 2-Letter grade using switch"<<"\n";
+		cout<<"Option 3-Exit"<<"\n";
 
-	cout<<"Choose an Option: ";
-	cin>>Option;
-	cout<<Option<<"\n";
+		cout<<"Choose an Option: ";
+		cin>>Option;
+		cout<<Option<<"\n";
 
-	if (Option == 1)
-	{
+		if (Option == 1)
+		{
 	
-		auto Grade = 0;
-		string result = "";
+			auto Grade = 0;
+			string result = "";
 
-		cout<<"Enter a Grade Between 0 and 100: ";
-		cin>>Grade;
+			cout<<"Enter a Grade Between 0 and 100: ";
+			cin>>Grade;
 
-		result = get_letter_grade_using_if(Grade);
+			result = get_letter_grade_using_if(Grade);
 
-		cout<<result<<"\n";
+			cout<<result<<"\n";
 
-		return 0;
-	}
+			//return 0;
+		}
 	
-	else if (Option == 2)
-	{
+		else if (Option == 2)
+		{
 	
-		auto Grade = 0;
-		string result = "";
+			auto Grade = 0;
+			string result = "";
 
-		cout<<"Enter a Grade Between 0 and 100: ";
-		cin>>Grade;
+			cout<<"Enter a Grade Between 0 and 100: ";
+			cin>>Grade;
 
-		result =  get_letter_grade_using_switch(Grade);
+			result =  get_letter_grade_using_switch(Grade);
 
-		cout<<result<<"\n";
+			cout<<result<<"\n";
 
-		return 0;
-	}
+			//return 0;
+		}
 
-	else if (Option == 3)
-	{
-		break;
-	}
+		else if (Option == 3)
+		{
+			break;
+		}
 	}
 }
