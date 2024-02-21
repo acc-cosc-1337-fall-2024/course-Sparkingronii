18e6da2ed5a6f358372b83ee758335cf4c2405f1
AlexanderPowersACC <157078028+Sparkingronii@users.noreply.github.com>
2024-02-21 00:07:52 +0000

    Homework 4 Revision?

--- src/homework/04_repetition/main.cpp
+++ src/homework/04_repetition/main.cpp
@@ -7,54 +7,59 @@ using std::cin; using std::cout;
 
 int main() 
 {
-	int Option;
-	cout<<"MENU"<<"\n";
-	cout<<"Option 1-Factorial"<<"\n";
-	cout<<"Option 2-Greates Common Divisor"<<"\n";
-	cout<<"Option 3-Exit"<<"\n";
-
-	cout<<"Choose an Option: ";
-	cin>>Option;
-	cout<<Option<<"\n";
-
-	if (Option == 1)
+	while(true)
 	{
+		int Option;
+		cout<<"MENU"<<"\n";
+		cout<<"Option 1-Factorial"<<"\n";
+		cout<<"Option 2-Greates Common Divisor"<<"\n";
+		cout<<"Option 3-Exit"<<"\n";
+
+		cout<<"Choose an Option: ";
+		cin>>Option;
+		cout<<Option<<"\n";
+
+		if (Option == 1)
+		{
 	
-		auto Number = 0;
-		auto result = 0;
+			auto Number = 0;
+			auto result = 0;
 
-		cout<<"Enter a Number: ";
-		cin>>Number;
+			cout<<"Enter a Number: ";
+			cin>>Number;
 
-		result = factorial(Number);
+			result = factorial(Number);
 
-		cout<<result<<"\n";
+			cout<<result<<"\n";
 
-		return 0;
-	}
+			//return 0;
+		}
 
-	if (Option == 2)
-	{
+		if (Option == 2)
+		{
 	
-		auto Number1 = 0;
-		auto Number2 = 0;
-		auto result = 0;
+			auto Number1 = 0;
+			auto Number2 = 0;
+			auto result = 0;
 
-		cout<<"Enter a Number: ";
-		cin>>Number1;
+			cout<<"Enter a Number: ";
+			cin>>Number1;
 
-		cout<<"Enter a Second Number: ";
-		cin>>Number2;
+			cout<<"Enter a Second Number: ";
+			cin>>Number2;
 
-		result = gcd(Number1, Number2);
+			result = gcd(Number1, Number2);
 
-		cout<<result<<"\n";
+			cout<<result<<"\n";
 
-		return 0;
-	}
+			//return 0;
+		}
 
-	else if (Option == 3)
-	{
-		return 0;
+		else if (Option == 3)
+		{
+			
+				break;
+				
+		}
 	}
 }
