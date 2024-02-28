e07cb49a3d09ce724b2ebb4613ca7e883e3c5b2e
AlexanderPowersACC <157078028+Sparkingronii@users.noreply.github.com>
2024-02-28 18:53:42 +0000

    Homework 5 part1

--- src/homework/05_functions/func.cpp
+++ src/homework/05_functions/func.cpp
@@ -1,3 +1,72 @@
 //add include statements
+#include<iostream>
+#include<string>
+using std::string;
 
 //add function code here
+
+double get_gc_content(string DNA)
+{
+int length = DNA.size();
+
+//char LetterG = 'G';
+int countG = 0;
+for (int i = 0; i < length; i++)
+    {
+        if (DNA[i] ==  'G')
+        {
+            ++ countG;
+        }
+    }
+
+//char LetterC = 'C';
+int countC = 0;
+for (int i = 0; i < length; i++)
+    {
+        if (DNA[i] ==  'C')
+        {
+            ++ countC;
+        }
+    }
+
+double GC_Content = countG + countC / length;
+return GC_Content;
+}
+
+
+string reverse_string(string DNA)
+{
+   int length = DNA.size();
+   int rev = length - 1;
+   for(int i=0; i<(length/2); i++)
+   {
+        char temp = DNA[i];
+        DNA[i] = DNA[rev];
+        DNA[rev] = temp;
+        rev = rev-1;
+   }
+    return DNA;
+}
+
+
+string get_dna_complement(string DNA)
+{
+int length = DNA.size();
+for(int i=0; i < length; i++)
+    {
+        if(DNA[i] == 'A')
+        DNA[i] = 'T';
+
+        else if(DNA[i] == 'T')
+        DNA[i] = 'A';
+
+        else if(DNA[i] == 'C')
+        DNA[i] = 'G';
+
+        else if(DNA[i] == 'G')
+        DNA[i] = 'C';
+
+        
+    }
+    return DNA;
+}
--- src/homework/05_functions/func.h
+++ src/homework/05_functions/func.h
@@ -1,1 +1,17 @@
 //add function(s) prototype here
+#include<iostream>
+#include<string>
+using std::string;
+
+
+//string DNA_Reference(const string& DNA);
+string DNA_Reference(const string& DNA);
+
+double get_gc_content(string DNA);
+
+string get_dna_complement(string DNA);
+
+string reverse_string(string DNA);
+
+
+
