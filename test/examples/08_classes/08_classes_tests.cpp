97aff8dd3f14230ac37a0ff8cae0de99a317781f
AlexanderPowersACC <157078028+Sparkingronii@users.noreply.github.com>
2024-03-29 00:08:36 +0000

    Classes p5

--- test/examples/08_classes/08_classes_tests.cpp
+++ test/examples/08_classes/08_classes_tests.cpp
@@ -1,22 +1,40 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "bank_account.h"
+#include "checking_account.h"
+#include "savings_account.h"
+
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 TEST_CASE("Test bank account get balance ")
 {
-	BankAccount account;
+	CheckingAccount account;
 	REQUIRE(account.get_balance() == 0);
 }
+
+TEST_CASE("Test savings account get balance")
+{
+	SavingsAccount account;
+	REQUIRE(account.get_balance() == 0);
+}
+
 TEST_CASE("Test bank account constructor with one parameter")
 {
-	BankAccount account(500);
+	CheckingAccount account(500);
+
 	REQUIRE(account.get_balance() == 500);
 }
+
+TEST_CASE("Test savings account with one constructor parameter")
+{
+	SavingsAccount account(500);
+	REQUIRE(account.get_balance() == 500);
+}
+
 TEST_CASE("Test case deposit with default constructor")
 {
-	BankAccount account;
+	CheckingAccount account;
 	REQUIRE(account.get_balance() == 0);
 	account.deposit(100);
 	REQUIRE(account.get_balance() == 100);
@@ -28,33 +46,26 @@ TEST_CASE("Test case with constructor")
 	account.deposit(100);
 	REQUIRE(account.get_balance() == 600);
 }
-
 TEST_CASE("Test withdraw with default constructor")
 {
 	BankAccount account;
 	REQUIRE(account.get_balance() == 0);
-
 	account.deposit(100);
 	REQUIRE(account.get_balance() == 100);
-
 	account.withdraw(20);
 	REQUIRE(account.get_balance() == 80);
 }
-
 TEST_CASE("Test withdraw with begin balance 500")
 {
 	BankAccount account(500);
 	REQUIRE(account.get_balance() == 500);
-
 	account.withdraw(50);
 	REQUIRE(account.get_balance() == 450);
 }
-
 TEST_CASE("Test withdraw more funds than balance")
 {
 	BankAccount account(100);
 	REQUIRE(account.get_balance() == 100);
-
 	account.withdraw(101);
 	REQUIRE(account.get_balance() == 100);
 }
