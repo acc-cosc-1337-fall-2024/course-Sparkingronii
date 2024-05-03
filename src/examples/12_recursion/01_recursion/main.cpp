e62c291320b6347b6b002cc44a2925353e1dcbdd
AlexanderPowersACC <157078028+Sparkingronii@users.noreply.github.com>
2024-05-03 00:28:47 +0000

    Recursion

--- src/examples/12_recursion/01_recursion/main.cpp
+++ src/examples/12_recursion/01_recursion/main.cpp
@@ -1,7 +1,14 @@
 #include "recursion.h"
+#include <iostream>
+using std::cout;
 
 int main() 
 {
-		
+	say_hello();
+	cout<<"Load to stack f(5)\n";
+	auto result = recursive_factorial(5);
+	cout<<"Unload from stack \n\n";
+	cout<<"Result: "<<result<<"\n";
+
 	return 0;
 }
